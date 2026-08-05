bd63edee686001d660a07264cfd8197c2a51a5c9
jgtans <jgtan@mail.ru>
2026-08-05 14:01:32 +0500

    инфраструктура: перевести проект на PostgreSQL

--- seatplan/settings.py
+++ seatplan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,15 @@ WSGI_APPLICATION = "seatplan.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "seatplan_db"),
+        "USER": os.environ.get("POSTGRES_USER", "seatplan_user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "seatplan_pass"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
