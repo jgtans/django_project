67ccde83d8e2f04bc9c739e5ca9be93ac74c41b0
jgtans <jgtan@mail.ru>
2026-08-07 08:50:38 +0500

    инфраструктура: перевести проект на PostgreSQL

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+class Workspace(models.Model):
+    number = models.CharField(max_length=10, verbose_name="Номер")
+    floor = models.IntegerField(verbose_name="Этаж")
+    workspace_type = models.CharField(max_length=50, verbose_name="Тип")
+
+    def __str__(self):
+        return f"Рабочее место {self.number}, этаж {self.floor}"
