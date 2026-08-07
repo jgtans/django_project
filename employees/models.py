cc0ce31bcc184cb0712bf31423e9591610d87fb5
jgtans <jgtan@mail.ru>
2026-08-07 19:02:52 +0500

    фича: добавить модель Employee с расчетом стажа

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    GENDER_CHOICES = [
+        ("M", "Мужской"),
+        ("F", "Женский"),
+    ]
+
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name="Пол")
+    hired_at = models.DateField(verbose_name="Дата трудоустройства")
+    description = models.TextField(blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
+        ordering = ["last_name"]
+
+    # Метод для расчета стажа
+    @property
+    def tenure_days(self):
+        from django.utils import timezone
+        delta = timezone.now().date() - self.hired_at
+        return delta.days
+
+#TODO добавить сбор метрик WorkSession() "10мин простоя-пауза"
--- workspaces/models.py
+++ workspaces/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 
 class Workspace(models.Model):
     number = models.CharField(max_length=10, verbose_name="Номер")
