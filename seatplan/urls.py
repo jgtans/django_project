94396b286b368bdd2224ea2486a204edaadca93c
jgtans <jgtan@mail.ru>
2026-08-07 08:33:46 +0500

    инфраструктура: 2_перевести проект на PostgreSQL

--- seatplan/urls.py
+++ seatplan/urls.py
@@ -1,5 +1,6 @@
 """
 URL configuration for seatplan project.
+.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -29,3 +30,6 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
     ]
+
+
+
