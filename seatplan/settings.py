315ed93255e17ba628d9bb300520b51826b8f5aa
jgtans <jgtan@mail.ru>
2026-08-10 00:39:28 +0500

    .

--- seatplan/settings.py
+++ seatplan/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,10 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "workspaces",
     "employees",
-
     "debug_toolbar",
     "django_extensions",
     "pydotplus",
